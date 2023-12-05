87ca0ff3e50939cf7f7970e31990d0f225583510
Jackson kanchanapally <92465955+kanchanapallyjackson@users.noreply.github.com>
2023-12-05 15:09:59 +0530

    sd

--- sap_AUTH/myapp/main.js
+++ sap_AUTH/myapp/main.js
@@ -1,21 +1,53 @@
 const express = require('express');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 const xsenv = require('@sap/xsenv');
 const JWTStrategy = require('@sap/xssec').JWTStrategy;
 
+const users = require('./users.json');
 const app = express();
 
-const services = xsenv.getServices({ uaa:'nodeuaa' });
+const services = xsenv.getServices({ uaa: 'nodeuaa' });
 
 passport.use(new JWTStrategy(services.uaa));
 
+app.use(bodyParser.json());
 app.use(passport.initialize());
 app.use(passport.authenticate('JWT', { session: false }));
 
-app.get('/', function (req, res, next) {
-  res.send('Application user: ' + req.user.id);
+app.get('/users', function (req, res) {
+  var isAuthorized = req.authInfo.checkScope('$XSAPPNAME.Display');
+  if (isAuthorized) {
+    res.status(200).json(users);
+  } else {
+    res.status(403).send('Forbidden');
+  }
 });
 
+app.post('/users', function (req, res) {
+  const isAuthorized = req.authInfo.checkScope('$XSAPPNAME.Update');
+  if (!isAuthorized) {
+    res.status(403).json('Forbidden');
+    return;
+  }
+
+  var newUser = req.body;
+  newUser.id = users.length;
+  users.push(newUser);
+
+  res.status(201).json(newUser);
+});
+// app.use(express.json())
+// app.get('/users',(req,res)=>{
+//   res.status(200).json(users);
+// })
+// app.post("/users",(req,res)=>{
+//   const name=req.body
+//   name.id=users.length
+//   users.push(name)
+//   res.status(200).json(users)
+
+// })
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('myapp listening on port ' + port);
